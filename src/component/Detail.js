6e5c811b7e17f4d4e05ef48370c46b470f413939
민형기 <alsvhtks@naver.com>
2022-12-26 18:55:37 +0900

    useEffect

--- src/component/Detail.js
+++ src/component/Detail.js
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export function Detail(props) {
+  let [alert, setAlert] = useState(true);
   let { id } = useParams();
-
+  let [num, setNum] = useState("");
+  let 찾은상품 = props.shoes.find(function (x) {
+    return x.id == id;
+  });
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+  });
+  useEffect(() => {
+    if (isNaN(num) == true) {
+      console.log("잘못입력");
+    }
+  }, [num]);
   return (
     <div className="container">
+      {alert == true ? <div className="alert">2초이내 구매시 할인</div> : null}
       <div className="row">
         <div className="col-md-6">
           <img
@@ -13,10 +29,15 @@ export function Detail(props) {
           />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">{props.shoes[id].title}</h4>
-          <p>{props.shoes[id].content}</p>
-          <p>{props.shoes[id].price}</p>
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}</p>
           <button className="btn btn-danger">주문하기</button>
+          <input
+            onChange={(e) => {
+              setNum(e.target.value);
+            }}
+          ></input>
         </div>
       </div>
     </div>
